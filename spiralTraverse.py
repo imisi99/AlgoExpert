caeeb0cddaaf730d56b4c24938c9737514d8782e
imisi99 <isongrichard234@gmail.com>
2025-01-25 23:47:08 +0100

    sprialTraverse

---
+++ spiralTraverse.py
@@ -0,0 +1,36 @@
+def spiralTraverse(array):
+    result = []
+    top = 0
+    left = 0
+    bottom = len(array) - 1
+    right = len(array[0]) - 1
+
+    while top <= bottom and left <= right:
+        # top
+        for i in range(left, right + 1):
+            result.append(array[top][i])
+
+        # right
+        for i in range(top + 1, bottom + 1):
+            result.append(array[i][right])
+
+        # bottom
+        for i in reversed(range(left, right)):
+            result.append(array[bottom][i])
+
+        # left
+        for i in reversed(range(top + 1, bottom)):
+            result.append(array[i][left])
+
+        top += 1
+        left += 1
+        bottom -= 1
+        right -= 1
+
+    return result
+
+test1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+test2 = [[1, 2, 3, 4, 5],
+         [10, 83, 93, 5, 6],
+         [7, 8, 9, 23, 32]]
+print(spiralTraverse(test2))
