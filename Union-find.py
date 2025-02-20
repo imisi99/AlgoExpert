2fde48475073655c269457a396f8fc7525f7f5c8
imisi99 <isongrichard234@gmail.com>
2025-02-20 18:21:36 +0100

    nextgreaterelement

--- Union-find.py
+++ Union-find.py
@@ -31,3 +31,39 @@ class UnionFind:
         for keys in self.holder[idx2].keys():
             self.holder[idx][keys] = value
         self.holder.pop(idx2)
+        
+        
+        
+class UnionFind:
+    def __init__(self):
+        self.parents = {}
+        self.rank = {}
+
+    def createSet(self, value):
+        self.parents[value] = value
+        self.rank[value] = 0
+        
+    def find(self, value):
+        if value not in self.parents:
+            return None
+            
+        if value != self.parents[value]:
+            self.parents[value] = self.find(self.parents[value])
+
+        return self.parents[value]
+        
+    def union(self, valueOne, valueTwo):
+        if valueOne not in self.parents or valueTwo not in self.parents:
+            return
+
+        valueOneRoot = self.find(valueOne)
+        valueTwoRoot = self.find(valueTwo)
+
+        if self.rank[valueOneRoot] < self.rank[valueTwoRoot]:
+            self.parents[valueOneRoot] = valueTwoRoot
+        elif self.rank[valueOneRoot] > self.rank[valueTwoRoot]:
+            self.parents[valueTwoRoot] = valueOneRoot
+        else:
+            self.parents[valueTwoRoot] = valueOneRoot
+
+        
